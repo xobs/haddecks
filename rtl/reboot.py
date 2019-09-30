f3d7322073f6adc254dfed455013ff377e4329cc
Sean Cross <sean@xobs.io>
2019-09-30 18:18:09 +0800

    reboot: make register name nicer
    Don't call it `REBOOT_REBOOT`.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- rtl/reboot.py
+++ rtl/reboot.py
@@ -6,8 +6,8 @@ from litex.soc.interconnect.csr import CSRStorage, CSRStatus, CSRField, AutoCSR
 
 class Reboot(Module, AutoCSR, AutoDoc):
     def __init__(self, pin):
-        self.reboot = CSRStorage(8, description="Write `0xac` here to reboot the device")
+        self.ctrl = CSRStorage(8, description="Write ``0xac`` here to reboot the device")
         self.intro = ModuleDoc(title="Reboot Control", body="""
         Access reboot control for the badge.  Write the key to ``reboot`` in
         order to initiate a reboot.""")
-        self.comb += If(self.reboot.storage != 0xac, pin.eq(1))
+        self.comb += If(self.ctrl.storage != 0xac, pin.eq(1))
