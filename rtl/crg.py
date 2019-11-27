17cfa5f29fd06e54563c4f98c20cbcd65195db96
Sean Cross <sean@xobs.io>
2019-11-27 18:00:20 +0800

    crg: add `slow` option
    The `slow` option runs sys at 12 MHz, which matches Fomu.  This avoids
    us having to do CDC in the USB stack, because it's not yet supported.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- rtl/crg.py
+++ rtl/crg.py
@@ -9,7 +9,7 @@ class _CRG(Module):
     Input: 8 MHz
     Output: 48 MHz
     """
-    def __init__(self, platform):
+    def __init__(self, platform, fast_sysclk=False):
         self.clock_domains.cd_por = ClockDomain(reset_less=True)
         self.clock_domains.cd_sys = ClockDomain()
         self.clock_domains.cd_clk12 = ClockDomain()
@@ -28,7 +28,10 @@ class _CRG(Module):
         clk8 = platform.request("clk8")
         # rst_n = platform.request("rst_n")
         platform.add_period_constraint(clk8, 1e9/8e6)
-        platform.add_period_constraint(self.cd_sys.clk, 1e9/48e6)
+        if fast_sysclk:
+            platform.add_period_constraint(self.cd_sys.clk, 1e9/48e6)
+        else:
+            platform.add_period_constraint(self.cd_sys.clk, 1e9/12e6)
         platform.add_period_constraint(self.cd_clk12.clk, 1e9/12e6)
         platform.add_period_constraint(self.cd_clk48.clk, 1e9/48e6)
 
@@ -42,25 +45,15 @@ class _CRG(Module):
         # pll
         self.submodules.pll = pll = ECP5PLL()
         pll.register_clkin(clk8, 8e6)
-        pll.create_clkout(self.cd_sys, 48e6, phase=0, margin=1e-9)
+        pll.create_clkout(self.cd_clk48, 48e6, phase=0, margin=1e-9)
         pll.create_clkout(self.cd_clk12, 12e6, phase=39, margin=1e-9)
-        self.comb += self.cd_clk48.clk.eq(self.cd_sys.clk)
-        # pll.create_clkout(self.cd_sys, 48e6, phase=0, margin=1e-9)
-        # pll.create_clkout(self.cd_clk12, 12e6, phase=132, margin=1e-9)
+        if fast_sysclk:
+            self.comb += self.cd_sys.clk.eq(self.cd_clk48.clk)
+        else:
+            self.comb += self.cd_sys.clk.eq(self.cd_clk12.clk)
 
         # Synchronize USB48 and USB12, and drive USB12 from USB48
         self.specials += [
-            # Instance("ECLKSYNCB",
-            #     i_ECLKI=self.cd_usb48_i.clk,
-            #     i_STOP=self.stop,
-            #     o_ECLKO=self.cd_usb48.clk),
-            # Instance("CLKDIVF",
-            #     p_DIV="2.0",
-            #     i_ALIGNWD=0,
-            #     i_CLKI=self.cd_usb48.clk,
-            #     i_RST=self.cd_usb48.rst,
-            #     o_CDIVX=self.cd_usb12.clk),
-            AsyncResetSynchronizer(self.cd_sys, ~por_done | ~pll.locked),# | ~rst_n),
+            AsyncResetSynchronizer(self.cd_clk48, ~por_done | ~pll.locked),# | ~rst_n),
             AsyncResetSynchronizer(self.cd_clk12, ~por_done | ~pll.locked),# | ~rst_n),
-            # AsyncResetSynchronizer(self.cd_usb48, ~por_done | ~pll.locked),# | ~rst_n)
         ]
